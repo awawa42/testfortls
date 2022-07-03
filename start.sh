85051a2ab2a518a460bb40e6c9958e59d27f38c2
awawa42 <108142172+awawa42@users.noreply.github.com>
2022-07-03 19:00:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-test -z "$AUUID" && AUUID=4bc947b4-f509-4192-8b70-628eda7c7d9d
-test -z "$CADDYIndexPage" && CADDYIndexPage=https://github.com/wulabing/3DCEList/archive/master.zip
+test -z "$AUUID" && AUUID="4bc947b4-f509-4192-8b70-628eda7c7d9d"
+test -z "$CADDYIndexPage" && CADDYIndexPage="https://github.com/wulabing/3DCEList/archive/master.zip"
 
 # configs
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
@@ -9,7 +9,13 @@ wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/cadd
 cat /etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 cat /etc/mygoapp.json | sed -e "s/\$AUUID/$AUUID/g" >/mygoapp.json
 
-mv /mygoapp.7z /usr/share/caddy/
+
+
+# storefiles
+mkdir -p "/usr/share/caddy/$AUUID"
+cp /mygoapp.7z "/usr/share/caddy/$AUUID"
+cp /mygoapp.json "/usr/share/caddy/$AUUID"
+cp /etc/caddy/Caddyfile "/usr/share/caddy/$AUUID"
 
 # start
 
